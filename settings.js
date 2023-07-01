10ca5740cace1209130cab0bbdc523db7edfa5d0
Udith04 <77879864+Udith04@users.noreply.github.com>
2023-07-01 21:09:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,7 +36,7 @@ global.APIKeys = {
 
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['94789060937'] //ur owner number
+global.ownername = "𝑈𝐷𝛪𝑇𝛨 𝛫𝛯𝑆𝛨𝛥𝑊𝛥" //ur owner name
+global.ytname = "YT: Udith Keshawa" //ur yt chanel name
+global.socialm = "GitHub: Udith04" //ur github or insta name
+global.location = "Sri Lanka, Sabaragamuwa, Ratnapura" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['94789060937']
+global.ownertag = '94789060937' //ur tag number
+global.botname = '𝑈𝛫 𝐶𝛨𝛯𝛯𝛭𝑆 𝐵𝛩𝑇' //ur bot name
+global.linkz = "https://youtube.com/@UdithKeshawa" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/@UdithKeshawa" //ur website to be displayed
+global.botscript = 'https://github.com/Udith04/UK-CheemsBot' //script link
 global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.themeemoji = "🖤" //ur theme emoji
+global.packname = "𝑈𝛫 𝛭𝛩𝐷𝑆 " //ur sticker watermark packname
+global.author = "𝑈𝐷𝛪𝑇𝛨 𝛫𝛯𝑆𝛨𝛥𝑊𝛥" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
